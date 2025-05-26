89be9012299a780eb90facce746a707a5a903689
kunal <kunalmahato11@gmail.com>
2025-05-26 21:47:52 +0530

    Adding income stats

--- mmclient/src/App.jsx
+++ mmclient/src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
 import avatar from './assets/chicken.png';
+import { PiPiggyBankFill } from "react-icons/pi";
+import { FaLongArrowAltUp } from "react-icons/fa";
+import { GiPayMoney } from "react-icons/gi";
+import { PiDotsSixBold } from "react-icons/pi";
+
 
 function App() {
  
@@ -26,6 +31,31 @@ function App() {
               </div>
             </div>
 
+
+            <div className="app-total">
+
+                <div className="income">
+                  <span className='income-title'>Total Income</span>
+                  <span className='income-value'>₹0</span>
+                  <span className="income-icon"><PiPiggyBankFill size={35}  /> <FaLongArrowAltUp /> </span>
+                </div>
+                <div className="expense">
+                  <span className='expense-title'>Total Expense</span>
+                  <span className='expense-value'>₹1</span>
+                  <div className="expense-icon"><GiPayMoney size={35} /></div>
+                </div>
+
+                <div className="app-total-icon"><PiDotsSixBold size={35} /></div>
+            </div>
+
+            <div className="app-history">
+              <div className="app-history__analytics">
+                <span>Analytics</span>
+              </div>
+              <div className="app-history__transactions">
+                <span>Transactions</span>
+              </div>
+            </div>
 
         </div>
       
